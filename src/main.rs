3b9f7011ac4a2daa2f710c20da14a8b0e642e385
DGriffin91 <github@dgdigital.net>
2022-11-24 17:20:29 -0800

    remove unused lights

--- src/main.rs
+++ src/main.rs
@@ -34,7 +34,8 @@ pub fn main() {
         .add_plugin(MipmapGeneratorPlugin)
         .add_system(generate_mipmaps::<StandardMaterial>)
         .add_startup_system(setup)
-        .add_system(remove_cameras)
+        .add_system(remove_default_cameras)
+        .add_system(remove_default_light)
         .add_system(flip_normals);
 
     app.run();
@@ -42,6 +43,9 @@ pub fn main() {
 
 #[derive(Component)]
 pub struct FlipNormals;
+
+#[derive(Component)]
+pub struct GrifLight;
 
 pub fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     println!("Loading models, generating mipmaps");
@@ -64,111 +68,123 @@ pub fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
 
     // Sun
     const HALF_SIZE: f32 = 20.0;
-    commands.spawn(DirectionalLightBundle {
-        transform: Transform::from_rotation(Quat::from_euler(
-            EulerRot::XYZ,
-            PI * -0.43,
-            PI * -0.08,
-            0.0,
-        )),
-        directional_light: DirectionalLight {
-            color: Color::rgb(1.0, 1.0, 0.99),
-            illuminance: 400000.0,
-            shadow_projection: OrthographicProjection {
-                left: -HALF_SIZE,
-                right: HALF_SIZE,
-                bottom: -HALF_SIZE,
-                top: HALF_SIZE,
-                near: -10.0 * HALF_SIZE,
-                far: 10.0 * HALF_SIZE,
-                ..default()
+    commands
+        .spawn(DirectionalLightBundle {
+            transform: Transform::from_rotation(Quat::from_euler(
+                EulerRot::XYZ,
+                PI * -0.43,
+                PI * -0.08,
+                0.0,
+            )),
+            directional_light: DirectionalLight {
+                color: Color::rgb(1.0, 1.0, 0.99),
+                illuminance: 400000.0,
+                shadow_projection: OrthographicProjection {
+                    left: -HALF_SIZE,
+                    right: HALF_SIZE,
+                    bottom: -HALF_SIZE,
+                    top: HALF_SIZE,
+                    near: -10.0 * HALF_SIZE,
+                    far: 10.0 * HALF_SIZE,
+                    ..default()
+                },
+                shadows_enabled: true,
+                shadow_depth_bias: 0.3,
+                shadow_normal_bias: 0.7,
             },
-            shadows_enabled: true,
-            shadow_depth_bias: 0.3,
-            shadow_normal_bias: 0.7,
-        },
-        ..default()
-    });
+            ..default()
+        })
+        .insert(GrifLight);
 
     // Sun Refl
-    commands.spawn(SpotLightBundle {
-        transform: Transform::from_xyz(2.0, -0.0, -2.0)
-            .looking_at(Vec3::new(0.0, 999.0, 0.0), Vec3::X),
-        spot_light: SpotLight {
-            range: 15.0,
-            intensity: 1000.0,
-            color: Color::rgb(1.0, 0.97, 0.85),
-            shadows_enabled: false,
-            inner_angle: PI * 0.4,
-            outer_angle: PI * 0.5,
+    commands
+        .spawn(SpotLightBundle {
+            transform: Transform::from_xyz(2.0, -0.0, -2.0)
+                .looking_at(Vec3::new(0.0, 999.0, 0.0), Vec3::X),
+            spot_light: SpotLight {
+                range: 15.0,
+                intensity: 1000.0,
+                color: Color::rgb(1.0, 0.97, 0.85),
+                shadows_enabled: false,
+                inner_angle: PI * 0.4,
+                outer_angle: PI * 0.5,
+                ..default()
+            },
             ..default()
-        },
-        ..default()
-    });
+        })
+        .insert(GrifLight);
 
     // Sun refl 2nd bounce / misc bounces
-    commands.spawn(SpotLightBundle {
-        transform: Transform::from_xyz(2.0, 5.5, -2.0)
-            .looking_at(Vec3::new(0.0, -999.0, 0.0), Vec3::X),
-        spot_light: SpotLight {
-            range: 13.0,
-            intensity: 800.0,
-            color: Color::rgb(1.0, 0.97, 0.85),
-            shadows_enabled: false,
-            inner_angle: PI * 0.3,
-            outer_angle: PI * 0.4,
+    commands
+        .spawn(SpotLightBundle {
+            transform: Transform::from_xyz(2.0, 5.5, -2.0)
+                .looking_at(Vec3::new(0.0, -999.0, 0.0), Vec3::X),
+            spot_light: SpotLight {
+                range: 13.0,
+                intensity: 800.0,
+                color: Color::rgb(1.0, 0.97, 0.85),
+                shadows_enabled: false,
+                inner_angle: PI * 0.3,
+                outer_angle: PI * 0.4,
+                ..default()
+            },
             ..default()
-        },
-        ..default()
-    });
+        })
+        .insert(GrifLight);
 
     // sky
     // seems to be making blocky artifacts. Even if it's the only light.
-    commands.spawn(PointLightBundle {
-        point_light: PointLight {
-            color: Color::rgb(0.8, 0.9, 0.97),
-            intensity: 100000.0,
-            shadows_enabled: false,
-            range: 24.0,
-            radius: 3.0,
+    commands
+        .spawn(PointLightBundle {
+            point_light: PointLight {
+                color: Color::rgb(0.8, 0.9, 0.97),
+                intensity: 100000.0,
+                shadows_enabled: false,
+                range: 24.0,
+                radius: 3.0,
+                ..default()
+            },
+            transform: Transform::from_xyz(0.0, 30.0, 0.0),
             ..default()
-        },
-        transform: Transform::from_xyz(0.0, 30.0, 0.0),
-        ..default()
-    });
+        })
+        .insert(GrifLight);
 
     // sky refl
-    commands.spawn(SpotLightBundle {
-        transform: Transform::from_xyz(0.0, -2.0, 0.0)
-            .looking_at(Vec3::new(0.0, 999.0, 0.0), Vec3::X),
-        spot_light: SpotLight {
-            range: 11.0,
-            intensity: 300.0,
-            color: Color::rgb(0.8, 0.9, 0.97),
-            shadows_enabled: false,
-            inner_angle: PI * 0.46,
-            outer_angle: PI * 0.49,
+    commands
+        .spawn(SpotLightBundle {
+            transform: Transform::from_xyz(0.0, -2.0, 0.0)
+                .looking_at(Vec3::new(0.0, 999.0, 0.0), Vec3::X),
+            spot_light: SpotLight {
+                range: 11.0,
+                intensity: 300.0,
+                color: Color::rgb(0.8, 0.9, 0.97),
+                shadows_enabled: false,
+                inner_angle: PI * 0.46,
+                outer_angle: PI * 0.49,
+                ..default()
+            },
             ..default()
-        },
-        ..default()
-    });
+        })
+        .insert(GrifLight);
 
     // sky low
-    commands.spawn(SpotLightBundle {
-        transform: Transform::from_xyz(3.0, 2.0, 0.0)
-            .looking_at(Vec3::new(0.0, -999.0, 0.0), Vec3::X),
-        spot_light: SpotLight {
-            range: 12.0,
-            radius: 0.0,
-            intensity: 1800.0,
-            color: Color::rgb(0.8, 0.9, 0.95),
-            shadows_enabled: false,
-            inner_angle: PI * 0.34,
-            outer_angle: PI * 0.5,
+    commands
+        .spawn(SpotLightBundle {
+            transform: Transform::from_xyz(3.0, 2.0, 0.0)
+                .looking_at(Vec3::new(0.0, -999.0, 0.0), Vec3::X),
+            spot_light: SpotLight {
+                range: 12.0,
+                radius: 0.0,
+                intensity: 1800.0,
+                color: Color::rgb(0.8, 0.9, 0.95),
+                shadows_enabled: false,
+                inner_angle: PI * 0.34,
+                outer_angle: PI * 0.5,
+                ..default()
+            },
             ..default()
-        },
-        ..default()
-    });
+        })
+        .insert(GrifLight);
 
     // Camera
     commands
@@ -200,9 +216,25 @@ pub fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
 }
 
 // Sponza has a bunch of camera by default
-pub fn remove_cameras(
+pub fn remove_default_cameras(
     mut commands: Commands,
     cameras: Query<Entity, (With<Camera>, Without<CameraController>)>,
+) {
+    for cam in &cameras {
+        commands.entity(cam).despawn();
+    }
+}
+
+// Sponza has a bunch of lights by default
+pub fn remove_default_light(
+    mut commands: Commands,
+    cameras: Query<
+        Entity,
+        (
+            Or<(With<PointLight>, With<DirectionalLight>, With<SpotLight>)>,
+            Without<GrifLight>,
+        ),
+    >,
 ) {
     for cam in &cameras {
         commands.entity(cam).despawn();
